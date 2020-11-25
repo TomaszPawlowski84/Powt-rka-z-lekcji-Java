0a0bec4748bf4ad5a1b8f6120307eb15e56dc9a4
TomaszPawlowski84 <tomaszpawlowski.kontakt@gmail.com>
2020-11-25 09:46:13 +0100

    TomaszP Pierwszy program

---
+++ Pierwszy program/src/com/TomaszPawlowski/Main.java
@@ -0,0 +1,12 @@
+package com.TomaszPawlowski;
+
+public class Main {             // Klasa -kontener (pomieszczenie) na aplikację.  Obejmuje składowe obiekty i metody
+                                //służące do wykonywania operacji na poziomie systemu, np. przesyłania danyc wyjściowych do konsoli.
+
+    public static void main(String[] args) {        //nagłówek metody. Oznacza on początek metody. Metodę można trakto-
+                                                    //  wać jak nazwaną grupę instrukcji programu. Main to nazwa metody.
+	// write your code here
+
+        System.out.println("Programowanie to świetna zabawa!");  //ten wiersz wyświetla na ekranie komunikat. "DRUKARKA"
+    }
+}
